3c20928a939ebac45e3a89d75821b7335f5999cf
dorg11 <dorg@wix.com>
2016-11-06 11:48:52 +0200

    a

--- public/app.js
+++ public/app.js
@@ -28,7 +28,7 @@ app.controller('change', function($scope) {
       $scope.res = $scope.url.replace(old, updated);
     };
     $scope.showInstance = function() {
-      $scope.instance = JSON.stringify($scope.parse(), undefined, 2);
+      $scope.instance = $scope.parse();
 
     }
     function getParameterByName(name, url) {
