205ee95ecf4f64e4096991ce309a2e095f4d97ca
Dorgalam <dorgalam@gmail.com>
2016-11-22 22:40:18 +0200

    changed to server request sender

--- public/controllers/hive.js
+++ public/controllers/hive.js
@@ -1,44 +1,20 @@
 angular.module('app').controller('hive', function($scope, $http) {
 
-  $scope.hive = {};
-  var hive = $scope.hive;
-  console.log(hive);
-  hive.url = 'https://openapi.wix.com/v1';
-  hive.showObject = function() {
+    $scope.hive = {};
+    var hive = $scope.hive;
     console.log(hive);
-  }
-  hive.getSignature = function() {
-    hive.timeStamp = new Date();
-    hive.timeStamp.toISOString();
-    $http({
-      method : "GET",
-      url : '/hiveSign?appId='+hive.appId+'&instanceId='+hive.instanceId+'&timeStamp='+hive.timeStamp+'&secretKey='+hive.secretKey+'&relativeUrl=/v1'+hive.relativeUrl,
-    }).then(function mySucces(response) {
-      hive.request = response.data;
-    }, function myError(response) {
-      $scope.request = response.statusText;
-    });
-  };
-
-  hive.makeRequest = function() {
-    $http({
-      method : "GET",
-      url : hive.url+hive.relativeUrl,
-      headers : {
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Methods' : 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Origin',
-        'x-wix-application-id':hive.appId,
-        'x-wix-instance-id':hive.instanceId,
-        'x-wix-timestamp':hive.timeStamp,
-        'x-wix-signature':hive.signature //signature that my server generated (equal sign is tossed)
-      }
-    }).then(function mySucces(response) {
-      hive.request = response.data;
-    }, function myError(response) {
-      $scope.request = response.statusText;
-    });
-  }
-
-
+    hive.url = 'https://openapi.wix.com/v1';
+    hive.showObject = function() {
+        console.log(hive);
+    }
+    hive.sendHiveGet = function() {
+        $http({
+            method: "GET",
+            url: '/hiveGet?appId=' + hive.appId + '&instanceId=' + hive.instanceId + '&secretKey=' + hive.secretKey + '&relativeUrl=/v1' + hive.relativeUrl,
+        }).then(function mySucces(response) {
+            hive.response = response.data;
+        }, function myError(response) {
+            hive.response = response.statusText;
+        });
+    };
 });
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
 var path = require('path');
+var request = require('request');
 
 app.use('/static', express.static('public'));
 app.use(bodyParser.json()); // for parsing application/json
@@ -22,28 +23,37 @@ if (process.env.OPENSHIFT_MONGODB_DB_URL) {
     var db = mongoose.connection;
 
     var schema = new mongoose.Schema({
-      body: {},
-      headers: {}
+        body: {},
+        headers: {}
     });
     var webhook = mongoose.model('webhook', schema);
 }
 
-app.get('/hive', function(req,res) {
-    res.sendFile(path.join(__dirname + '/hive.html'));  
+app.get('/hive', function(req, res) {
+    res.sendFile(path.join(__dirname + '/hive.html'));
 });
 
-app.get('/hiveSign', function(req, res) {
-    var headers = 'GET\n';
-    headers += req.query.relativeUrl;
-    headers += '\n1.0.0\n';
-    headers += req.query.appId;
-    headers += '\n';
-    headers += req.query.instanceId;
-    headers += '\n';
-    headers += req.query.timeStamp;
-    var hmac = crypto.createHmac('sha256', req.query.secretKey );
-    var newSignature = hmac.update(headers).digest('base64').replace(/\+/g, '-').replace(/\//g, '_'); //different base64 standards
-    res.send(newSignature);
+app.get('/hiveGet', function(req, res) {
+    var hiveReq = {};
+    hiveReq.timeStamp = new Date();
+    hiveReq.timeStamp = hiveReq.timeStamp.toISOString();
+    hiveReq.url = 'https://openapi.wix.com';
+    hiveReq.headers = 'GET\n' + req.query.relativeUrl + '\n1.0.0\n' + req.query.appId + '\n' + req.query.instanceId + '\n' + hiveReq.timeStamp;
+    hiveReq.signature = crypto.createHmac('sha256', req.query.secretKey);
+    hiveReq.signature = hiveReq.signature.update(hiveReq.headers).digest('base64').replace(/\+/g, '-').replace(/\//g, '_'); //different base64 standards
+    hiveReq.options = {
+        url: hiveReq.url + req.query.relativeUrl + '?version=1.0.0',
+        headers: {
+            'x-wix-application-id': req.query.appId,
+            'x-wix-instance-id': req.query.instanceId,
+            'x-wix-timestamp': hiveReq.timeStamp,
+            'x-wix-signature': hiveReq.signature //signature that my server generated (equal sign is tossed)
+        }
+    }
+    request.get(hiveReq.options, function(error, response, body) {
+        res.send(response.body);
+    });
+
 });
 
 app.post('/server', function(req, res) {
@@ -63,8 +73,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/change',function(req,res){
-  res.redirect('/instance');
+app.get('/change', function(req, res) {
+    res.redirect('/instance');
 });
 
 app.get('/instance', function(req, res) {
@@ -73,7 +83,7 @@ app.get('/instance', function(req, res) {
 app.get('/sign', function(req, res) {
     var data = req.query.data;
     var hmac = crypto.createHmac('sha256', req.query.signature);
-    res.send(hmac.update(data).digest('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g,''));
+    res.send(hmac.update(data).digest('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, ''));
 });
 app.get('/get', function(req, res) {
     webhook.find({}, function(err, data) {
