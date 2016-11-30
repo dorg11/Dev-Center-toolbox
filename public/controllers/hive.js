06acfbca7122c933db31fbefc19c67653ee35839
dorg11 <dorg@wix.com>
2016-11-30 15:20:18 +0200

    rick background

--- public/controllers/hive.js
+++ public/controllers/hive.js
@@ -8,7 +8,7 @@ angular.module('app').controller('hive', function($scope, $http) {
         console.log(hive);
     }
     hive.sendHiveGet = function() {
-        hive.getUrl = '/hiveGet?appId=' + hive.appId + '&instanceId=' + hive.instanceId + '&secretKey=' + hive.secretKey + '&relativeUrl=/v1' + hive.relativeUrl  + (hive.queryParams ? '&queryParams=' + hive.queryParams : '') + '&requestType=' + hive.requestType;
+        hive.getUrl = '/hiveGet?appId=' + hive.appId + '&instanceId=' + hive.instanceId + '&secretKey=' + hive.secretKey + '&relativeUrl=/v1' + hive.relativeUrl + (hive.queryParams ? '&queryParams=' + hive.queryParams : '') + '&requestType=' + hive.requestType;
         $http({
             method: "GET",
             url: hive.getUrl,
@@ -18,87 +18,83 @@ angular.module('app').controller('hive', function($scope, $http) {
             hive.response = response.statusText;
         });
     };
-    hive.request = [
-      {
-        "GET" : ['/contacts', '/contacts/search','/contacts/mailingList' , '/activities', '/activities/types', '/labels', '/sites/site', '/sites/site/contributors', '/sites/site/pages', '/sites/site/settings', '/service/actions/email/providers', '/billing/products', '/billing/active', '/redirects']
-      },
-      {
-        "POST" : ['/contacts', '/activities', '/labels', '/notifications', '/services/actions/email', '/services/actions/email/single', '/services/actions/done', '/bulk/contacts', '/batch']
-      },
-      {
-        "PUT" : ['NOT SUPPORTED CURRENTLY']
-      }
-    ]
-  /*  hive.requests = {
-      "GET" : {
-        "/contacts" : {
-          "contactId" : {
-            "type" : "path",
-            "message" : "Not mandatory"
-          }
-        },
-        "/contacts/search" : {
-            "q" : {
+    hive.requestTypes = ['GET', 'POST', 'PUT'];
+    hive.request = {
+        "GET": ['/contacts', '/contacts/search', '/contacts/mailingList', '/activities', '/activities/types', '/labels', '/sites/site', '/sites/site/contributors', '/sites/site/pages', '/sites/site/settings', '/service/actions/email/providers', '/billing/products', '/billing/active', '/redirects'],
+        "POST": ['/contacts', '/activities', '/labels', '/notifications', '/services/actions/email', '/services/actions/email/single', '/services/actions/done', '/bulk/contacts', '/batch'],
+        "PUT": ['NOT SUPPORTED CURRENTLY']
+    }
+    
+    /*  hive.requests = {
+        "GET" : {
+          "/contacts" : {
+            "contactId" : {
+              "type" : "path",
+              "message" : "Not mandatory"
+            }
+          },
+          "/contacts/search" : {
+              "q" : {
+                "type" : "path"
+              }
+          },
+          "/contacts/mailingList" : null,
+          "/contacts/{contactId}/activities" : {
+            "contactId" : {
               "type" : "path"
             }
-        },
-        "/contacts/mailingList" : null,
-        "/contacts/{contactId}/activities" : {
-          "contactId" : {
-            "type" : "path"
+          },
+          "/activities/{activityId}" : {
+            "activityId" : {
+              "type" : "path",
+              "message" : "Not mandatory"
+            }
+          },
+          "/activities/types" : null,
+          "/labels" : {
+            "labelId" : {
+              "type" : "path",
+              "message" : "Not mandatory"
+            }
+          },
+          "/labels/{labelId}/contacts" : {
+
+          },
+          "/sites/site" : {
+
+          },
+          "/sites/site/contributors" : {
+
+          },
+          "/sites/site/pages" : {
+
+          },
+          "/sites/site/settings" : {
+
+          },
+          "/services/actions/email/providers" : {
+
+          },
+          "/billing/products" : {
+
+          },
+          "/billing/active" : {
+
+          },
+          "/billing/products/{productId}" : {
+
+          },
+          "/redirects" : {
+
           }
         },
-        "/activities/{activityId}" : {
-          "activityId" : {
-            "type" : "path",
-            "message" : "Not mandatory"
-          }
-        },
-        "/activities/types" : null,
-        "/labels" : {
-          "labelId" : {
-            "type" : "path",
-            "message" : "Not mandatory"
-          }
-        },
-        "/labels/{labelId}/contacts" : {
+        "POST" : {
 
         },
-        "/sites/site" : {
-
-        },
-        "/sites/site/contributors" : {
-
-        },
-        "/sites/site/pages" : {
-
-        },
-        "/sites/site/settings" : {
-
-        },
-        "/services/actions/email/providers" : {
-
-        },
-        "/billing/products" : {
-
-        },
-        "/billing/active" : {
-
-        },
-        "/billing/products/{productId}" : {
-
-        },
-        "/redirects" : {
+        "PUT" : {
 
         }
-      },
-      "POST" : {
-
-      },
-      "PUT" : {
-
-      }
-    }*/
+      }*/
 
 
 
